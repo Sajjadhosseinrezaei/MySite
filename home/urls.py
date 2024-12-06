b903fa9cd8424caa8819fbc4937921d4eed54b6a
sajjadhossein <sajjadhosseinrezaei@yahoo.com>
2024-12-06 20:01:14 +0330

    add front end to my site

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
+app_name = 'home'
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home/home.html'), name='home'),
+    path('about', TemplateView.as_view(template_name='home/about.html'), name='about'),
+    path('resume', TemplateView.as_view(template_name='home/resume.html'), name='resume'),
+    path('services', TemplateView.as_view(template_name='home/services.html'), name='services'),
+    path('portfolio', TemplateView.as_view(template_name='home/portfolio.html'), name='portfolio'),
+    path('contact', TemplateView.as_view(template_name='home/contact.html'), name='contact'),
 ]
