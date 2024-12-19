9c64b5212c8bc46c211440fbf256b009878b267a
Sajjadhosseinrezaei <146534587+Sajjadhosseinrezaei@users.noreply.github.com>
2024-12-19 23:30:30 +0330

    Update settings.py for pythnanywhere 3

--- MySite/settings.py
+++ MySite/settings.py
@@ -113,8 +113,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 import os
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = '/home/sajjadhossein/sajjadhossein.pythonanywhere.com/static'
+STATICFILES_DIRS = [BASE_DIR / 'static'
+STATIC_ROOT = '/home/sajjadhossein/sajjadhossein.pythonanywhere.com/staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
